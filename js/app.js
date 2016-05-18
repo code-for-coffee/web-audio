6437b91047614b1acdf1a9917013a5b0aac750ec
code-for-coffee <jtraverjr@gmail.com>
2016-05-17 20:11:21 -0500

    web audio test

--- js/app.js
+++ js/app.js
@@ -1,1 +1,17 @@
+// project namespace
+var webAudio = webAudio || {};
+webAudio.context;
+webAudio.initialize = function webAudioInitialize() {
+  try {
+    window.AudioContext = window.AudioContext || window.webkitAudioContext;
+    webAudio.context = new AudioContext();
+  } catch (ex) {
+    console.log('Web Audio API is not supported on this device.');
+  } finally {
+    console.log('Web Audio API is supported on this device.');
+  }
+};
+window.addEventListener('load', webAudio.initialize, false);
+
 $(document).foundation();
+
